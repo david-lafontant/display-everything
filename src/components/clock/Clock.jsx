5d7e0e1f85e6aea78cf393e044ac20f5749ba4e0
david-lafontant <lafontant.david@gmail.com>
2023-01-09 15:43:52 -0600

    Add falsy case

--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const Clock = props => {
-  return (
-    <div>Clock</div>
-  )
+const Clock = ({value}) => {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000);
+  }, []);
+
+  if (!value) {
+    return (
+
+      <p>
+        {date.toLocaleString('en-US', {
+          hour: 'numeric',
+          minute: 'numeric',
+          hour12: true,
+        })}
+      </p>
+    )
+  }
 }
 
 Clock.propTypes = {}
 
-export default Clock
+export default Clock;
