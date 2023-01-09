257febeb4572e564e1256d0c714cc7dbdd47c738
david-lafontant <lafontant.david@gmail.com>
2023-01-09 16:48:31 -0600

    Fix component path in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
-import Clock from './components/clock/Clock'
+import Everything from './components/everything/Everything'
 
 function App() {
- const value = 0;
+
+  // The variable value (line 7) is the props to be passed to the everything component.
+  // it can be an array (ex: const value = [1,2,3,'dan']), a string or a number.
+  // it can be object (ex: const value= {age: 1, weight: 50, name: 'Dan'}).
+  // it can also be any falsy value (as defined in https://developer.mozilla.org/en-US/docs/Glossary/Falsy)
+ 
+  const value = null;
 
   return (
     <div className="App">
-      <Clock value={value} />
+      <Everything value={value} />
     </div>
   )
 }
