95c68ce8b2ebf60267ce04af422888eaa3c3001c
david-lafontant <lafontant.david@gmail.com>
2023-01-09 15:21:21 -0600

    Import and display of Clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import Clock from './components/clock/Clock'
 
 function App() {
-
+ const value = 0;
 
   return (
     <div className="App">
-      
+      <Clock value={value} />
     </div>
   )
 }
